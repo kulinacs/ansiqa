7be8586f08ae10507afd49c02dbda8597a61eb96
Nicklaus McClendon <nicklaus@kulinacs.com>
2017-05-02 20:49:50 -0500

    Added pygraphviz dependency

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
 
     packages=find_packages(),
 
-    install_requires=['ruamel.yaml', 'argparse', 'tabulate', 'colored', 'jinja2'],
+    install_requires=['ruamel.yaml', 'argparse', 'tabulate', 'colored', 'jinja2', 'pygraphviz'],
 
     entry_points={
         'console_scripts': [
